9d927ca19d8955fd0b40aac9eefc8dd020802e77
William McIntosh <46854008+williammcintosh@users.noreply.github.com>
2021-06-26 23:52:13 -0700

    Add files via upload

---
+++ Unity_scripts/ChartScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChartScript : MonoBehaviour
+{
+    public float averageScore;
+    public float generation;
+    public float totalIterations;
+    public float xRatio;
+    public Text TwentyPct, FortyPct, SixtyPct, EightyPct, HundredPct;
+    public GameObject dotPrefab;
+    GameObject myDot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UpdateValues(float theAverageScore, float theGeneration)
+    {
+        averageScore = theAverageScore;
+        float currentIteration = (float) theGeneration;
+        xRatio = 400/totalIterations;
+        //Debug.Log("["+(currentIteration*xRatio).ToString()+","+(averageScore*14.25f).ToString()+"]");
+        PlaceDot((float) currentIteration*xRatio, averageScore*14.25f);
+    }
+    public void PlaceDot(float xCor, float yCor)
+    {
+        Vector2 pos = new Vector2(this.transform.position.x+xCor, this.transform.position.y+yCor);
+        myDot = Instantiate(dotPrefab, pos, Quaternion.identity) as GameObject;
+        myDot.transform.SetParent(this.transform, true);
+    }
+    public void SetNumPop(int thePop)
+    {
+        totalIterations = thePop; 
+        TwentyPct.text = ((totalIterations / 5) * 1).ToString();
+        FortyPct.text = ((totalIterations / 5) * 2).ToString();
+        SixtyPct.text = ((totalIterations / 5) * 3).ToString();
+        EightyPct.text = ((totalIterations / 5) * 4).ToString();
+        HundredPct.text = totalIterations.ToString();
+    }
+}
---
+++ Unity_scripts/TileScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileScript : MonoBehaviour
+{
+    public Text myText;
+    public string myTextString;
+    public Image mybackground;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void UpdateMyText(string newString)
+    {
+        myText.text = newString;
+    }
+    public void UpdateMyBackGround(int count)
+    {
+        if (count%2 == 0) 
+            mybackground.color = new Color32(80,80,80,255);
+        else
+            mybackground.color = new Color32(120,120,120,255);
+    }
+}
